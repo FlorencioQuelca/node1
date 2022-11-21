8055a20c046b31a928631c36bc1080dd8d0de946
FlorencioQuelca <florens.lonwe@gmail.com>
2022-11-20 23:06:45 -0400

    primer avance

--- courses.sql
+++ courses.sql
@@ -1,1 +1,31 @@
-hola
+CREATE TABLE "users" (
+  "id" uuid PRIMARY KEY,
+  "name" varchar,
+  "email" varchar,
+  "password" varchar,
+  "age" integer
+);
+
+CREATE TABLE "courses" (
+  "id" uuid PRIMARY KEY,
+  "title" varchar,
+  "desciption" text,
+  "level" varchar,
+  "teacher" varchar
+);
+
+CREATE TABLE "videos" (
+  "id" uuid PRIMARY KEY,
+  "title" varchar,
+  "url" varchar
+);
+
+CREATE TABLE "categories" (
+  "id" uuid PRIMARY KEY,
+  "name" varchar
+);
+
+CREATE TABLE "roles" (
+  "id" uuid PRIMARY KEY,
+  "name" varchar
+);
